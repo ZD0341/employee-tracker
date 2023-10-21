d5e59c188750d0437da9d3f418bc9262a368add2
ZD0341 <144489565+ZD0341@users.noreply.github.com>
2023-10-21 03:23:08 -0700

    mysql2

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-in
+const inquirer = require('inquirer');
 const mysql = require('mysql2');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
+const cTable = require('console.table');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -17,7 +15,15 @@ const db = mysql.createConnection(
   console.log(`Connected to the classlist_db database.`)
 );
 
-// Query database
-db.query('SELECT * FROM students', function (err, results) {
-  console.log(results);
-});
+// execute will internally call prepare and query
+connection.execute(
+  'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+  ['Rick C-137', 53],
+  function(err, results, fields) {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+
+    // If you execute same statement again, it will be picked from a LRU cache
+    // which will save query preparation time and give better performance
+  }
+);
