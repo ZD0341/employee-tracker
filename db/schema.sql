158836e9a2cd2d6c73c761f03923cd1eb46a3499
ZD0341 <144489565+ZD0341@users.noreply.github.com>
2023-10-23 12:28:39 -0700

    made insert statements

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,27 @@
-DROP DATABASE IF EXISTS emp_tracker;
-CREATE DATABASE emp_tracker;
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
 
-USE emp_tracker;
-SELECT DATABASE();
+USE tracker_db;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
 );
 
 
 CREATE TABLE role (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL
+  salary DECIMAL,
   department_id INT
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
-  -- /  FOREIGN KEY (instructor_id) REFERENCES instructors(id) ON DELETE SET NULL
+  last_name VARCHAR(30)
+  role_id INT
+  manager_id INT
+  -- /  FOREIGN KEY (instructor_id) REFERENCES instructors(id) ON DELETE CASCADE
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,20 @@
-INSERT INTO students (first_name, last_name, enrolled)
-VALUES ("Elliot", "Smith", false),
-       ("Amira", "Afzal", true),
-       ("Christoper", "Lee", true),
-       ("Verónica", "Rodriguez", false),
-       ("Igor", "Ivanov", true);
-       
+INSERT INTO departments (name) 
+VALUES
+  ('HR'),
+  ('Engineering'),
+  ('Marketing'),
+  ('Sales');
+
+INSERT INTO roles (title, salary, department_id) 
+VALUES
+  ('HR Manager', 60000, 1),
+  ('Software Engineer', 80000, 2),
+  ('Marketing Specialist', 55000, 3),
+  ('Sales Manager', 70000, 4);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id) 
+VALUES
+  ('Zeke', 'Doe', 1, NULL),
+  ('Gabriel', 'Johnson', 2, NULL),
+  ('Quinn', 'Smith', 3, NULL),
+  ('Bolt', 'Williams', 4, NULL);
